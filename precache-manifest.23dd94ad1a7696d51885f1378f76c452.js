0833c02cb500efbfac1728aefd3feb9efffc57c1
Divyanshu Agrawal <divyanshu123@hotmail.com>
2019-10-10 17:23:44 +0530

    Updates

---
+++ precache-manifest.23dd94ad1a7696d51885f1378f76c452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a14b50dc8f169367d5b12ffc43023b6",
+    "url": "/hereisdx/sprint-ecommerce/index.html"
+  },
+  {
+    "revision": "7490986817c5a0215081",
+    "url": "/hereisdx/sprint-ecommerce/static/css/main.7f76ec66.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/hereisdx/sprint-ecommerce/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "7490986817c5a0215081",
+    "url": "/hereisdx/sprint-ecommerce/static/js/main.10110f71.chunk.js"
+  },
+  {
+    "revision": "2f9fec11c3a312ff842f",
+    "url": "/hereisdx/sprint-ecommerce/static/js/runtime~main.6d719715.js"
+  }
+]);
